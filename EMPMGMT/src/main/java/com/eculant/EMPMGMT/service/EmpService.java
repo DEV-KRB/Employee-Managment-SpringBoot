d02fdf229f3b9c43fb0f437c3bffb80bcb406f01
Kavya Bansore <kavya.bansore25@gmail.com>
2023-08-02 21:00:10 +0530

    final commit

--- EMPMGMT/src/main/java/com/eculant/EMPMGMT/service/EmpService.java
+++ EMPMGMT/src/main/java/com/eculant/EMPMGMT/service/EmpService.java
@@ -9,4 +9,8 @@ public interface EmpService {
 	public List<Employee> showAll();
 	
 	public Employee findById(int theId);
+	
+	public void save(Employee theEmployee);
+	
+	public void deleteEmpById(int theId);
 }
--- EMPMGMT/src/main/java/com/eculant/EMPMGMT/service/EmpServiceImpl.java
+++ EMPMGMT/src/main/java/com/eculant/EMPMGMT/service/EmpServiceImpl.java
@@ -1,37 +1,58 @@
 package com.eculant.EMPMGMT.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import com.eculant.EMPMGMT.dao.EmployeeDao;
+import com.eculant.EMPMGMT.dao.EmployeeRepository;
 import com.eculant.EMPMGMT.entity.Employee;
 
 @Service
 public class EmpServiceImpl implements EmpService {
 
-	private EmployeeDao employeeDao;
+	private EmployeeRepository employeeRepository;
 	
 	@Autowired
-	public EmpServiceImpl(EmployeeDao theEmployeeDao) {
+	public EmpServiceImpl(EmployeeRepository theEmployeeRepository) {
 		
-		employeeDao = theEmployeeDao;
+		employeeRepository = theEmployeeRepository;
 	}
 	
 	@Override
-	@Transactional
 	public List<Employee> showAll() {
 		// TODO Auto-generated method stub
-		return employeeDao.showAll();
+		return employeeRepository.findAll();
 	}
 
 	@Override
-	@Transactional
 	public Employee findById(int theId) {
 		// TODO Auto-generated method stub
-		return employeeDao.findEmpById(theId);
+		Optional<Employee> result = employeeRepository.findById(theId);
+		
+		Employee theEmployee = null;
+		if(result.isPresent()) {
+			return result.get();
+		}
+		else {
+			System.out.println("not found");
+		}
+		return theEmployee ;
+	}
+
+	@Override
+	public void save(Employee theEmployee) {
+		
+		employeeRepository.save(theEmployee);
+		
+	}
+
+	@Override
+	public void deleteEmpById(int theId) {
+		
+		employeeRepository.deleteById(theId);
+		
 	}
 
 }
